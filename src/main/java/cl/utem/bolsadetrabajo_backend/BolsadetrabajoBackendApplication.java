c8d39d1b3adc5f147c3270eb0ace3302e90950de
ddiaz <dylandb1998@gmail.com>
2025-05-26 21:49:17 -0400

    feat: initial development commit

---
+++ src/main/java/cl/utem/bolsadetrabajo_backend/BolsadetrabajoBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.utem.bolsadetrabajo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsadetrabajoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsadetrabajoBackendApplication.class, args);
+	}
+
+}
