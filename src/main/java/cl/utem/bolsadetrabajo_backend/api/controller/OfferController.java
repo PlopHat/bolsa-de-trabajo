d8eb877260812635ab1b41d34b609e99d7b4fa28
ddiaz <dylandb1998@gmail.com>
2025-05-26 22:49:32 -0400

    feat: adds jpa configuration and some other confs

---
+++ src/main/java/cl/utem/bolsadetrabajo_backend/api/controller/OfferController.java
@@ -0,0 +1,10 @@
+package cl.utem.bolsadetrabajo_backend.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/offers")
+public class OfferController {
+
+}
