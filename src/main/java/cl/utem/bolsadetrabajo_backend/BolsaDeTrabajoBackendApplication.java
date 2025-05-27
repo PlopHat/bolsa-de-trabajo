aace4c6800b006f0a20fedb04f9cbb4f3544659f
ddiaz <dylandb1998@gmail.com>
2025-05-26 22:41:48 -0400

    feat: enable jpa auditing

--- src/main/java/cl/utem/bolsadetrabajo_backend/BolsaDeTrabajoBackendApplication.java
+++ src/main/java/cl/utem/bolsadetrabajo_backend/BolsaDeTrabajoBackendApplication.java
@@ -2,8 +2,10 @@ package cl.utem.bolsadetrabajo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BolsaDeTrabajoBackendApplication {
 
 	public static void main(String[] args) {
