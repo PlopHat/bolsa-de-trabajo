0be1a94f0c5fdbe16855904ba266a682f3438acd
ddiaz <dylandb1998@gmail.com>
2025-05-27 21:56:25 -0400

    feat: added controller for offers

--- src/main/java/cl/utem/bolsadetrabajo_backend/api/controller/OfferController.java
+++ src/main/java/cl/utem/bolsadetrabajo_backend/api/controller/OfferController.java
@@ -1,10 +1,24 @@
 package cl.utem.bolsadetrabajo_backend.api.controller;
 
+import cl.utem.bolsadetrabajo_backend.api.dto.response.OfferResponse;
+import cl.utem.bolsadetrabajo_backend.service.OfferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "api/offers")
 public class OfferController {
+  @Autowired
+  private OfferService offerService;
+
+  public ResponseEntity<List<OfferResponse>> getAllOffers() {
+
+    return ResponseEntity.status(HttpStatus.OK).body(offerService.getOffers());
+  }
 
 }
