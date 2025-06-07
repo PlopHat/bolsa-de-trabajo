de87a8c80f865811abd4af41bc49fe27a3e95387
ddiaz <dylandb1998@gmail.com>
2025-06-07 15:07:50 -0400

    feat: Added OdderApplication Dtos

---
+++ src/main/java/cl/utem/bolsadetrabajo_backend/api/dto/request/OfferApplicationRequest.java
@@ -0,0 +1,24 @@
+package cl.utem.bolsadetrabajo_backend.api.dto.request;
+
+import cl.utem.bolsadetrabajo_backend.domain.entity.enums.OfferApplicationStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OfferApplicationRequest {
+  @JsonProperty(value = "offerId")
+  @NotNull(message = "offerId must not be null")
+  private Long offerId;
+
+  @JsonProperty(value = "requestDate")
+  @NotNull(message = "requestDate must not be null")
+  private LocalDateTime RequestDate;
+
+  @JsonProperty(value = "offerApplicationStatus")
+  @NotNull(message = "offerApplicationStatus must not be null")
+  private OfferApplicationStatus offerApplicationStatus;
+
+}
--- src/main/java/cl/utem/bolsadetrabajo_backend/api/dto/response/OfferApplicationDto.java
+++ src/main/java/cl/utem/bolsadetrabajo_backend/api/dto/response/OfferApplicationDto.java
@@ -1,8 +1,10 @@
 package cl.utem.bolsadetrabajo_backend.api.dto.response;
 
+import cl.utem.bolsadetrabajo_backend.domain.entity.OfferApplication;
 import cl.utem.bolsadetrabajo_backend.domain.entity.enums.OfferApplicationStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Value;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -10,17 +12,41 @@ import java.time.LocalDateTime;
 /**
  * DTO for {@link cl.utem.bolsadetrabajo_backend.domain.entity.OfferApplication}
  */
-@Value
+@Data
 public class OfferApplicationDto implements Serializable {
+  @JsonProperty(value = "id")
   Long id;
+
+  @JsonProperty(value = "createdAt")
   LocalDateTime createdAt;
+
+  @JsonProperty(value = "updatedAt")
   LocalDateTime updatedAt;
+
+  @JsonProperty(value = "user")
   @NotNull(message = "user must exist on an offer application")
   UserResponse User;
+
+  @JsonProperty(value = "offerApplicationRequest")
   @NotNull(message = "offer application must have an offer")
   OfferResponse offerApplicationRequest;
+
+  @JsonProperty(value = "requestDate")
   @NotNull(message = "offer application must have an request date")
   LocalDateTime RequestDate;
+
+  @JsonProperty(value = "offerApplicationStatus")
   @NotNull(message = "offer application must have an application status")
   OfferApplicationStatus offerApplicationStatus;
+
+  OfferApplicationDto toDto(OfferApplication offerApplication) {
+    this.id = offerApplication.getId();
+    this.createdAt = offerApplication.getCreatedAt();
+    this.updatedAt = offerApplication.getUpdatedAt();
+    this.User = new UserResponse().toDto(offerApplication.getUser());
+    this.offerApplicationRequest = new OfferResponse().toDto(offerApplication.getOfferApplicationRequest());
+    this.RequestDate = offerApplication.getRequestDate();
+    this.offerApplicationStatus = offerApplication.getOfferApplicationStatus();
+    return this;
+  }
 }
